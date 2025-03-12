a9ab0b80564afea8677d3da5392052106abe5a8c
Yah-Mo1 <yahyamohamed1231@outlook.com>
2025-03-12 12:39:41 +0000

    pushing notes

--- Bash-Scripting/arena/lv13.sh
+++ Bash-Scripting/arena/lv13.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+# Mission: Create a script that backs up a directory to a specified location and keeps only the last 5 backups.
+
+source_directory="Arena"
+dest_directory="backup"
+
+# Check if directory exists
+if [[ ! -d "$source_directory" ]]; then
+    echo "Error: Configuration file '$config_directory' not found!"
+    exit 1
+fi
+
+day=$(date +%A)
+hostname=$(hostname -s)
+archive="$hostname-$day.tgz"
 
 
-rsync -a $file 
+tar czf $dest_directory/$archive $source_directory
+
+
+echo "backup done!"
+
+cd backup
+ls -t | head -n -5 | xargs --no-run-if-empty rm
+
